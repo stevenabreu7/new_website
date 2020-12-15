a891b4a77de448c29d163df898ad82f8658628f7
Steven Abreu <stevenabreu7@gmail.com>
2020-12-15 20:08:14 +0100

    container width change

--- src/components/ui/Container/index.tsx
+++ src/components/ui/Container/index.tsx
@@ -6,8 +6,8 @@ interface Props extends Styled.StyledProps {
   children: React.ReactNode;
 }
 
-const Container: React.FC<Props> = ({ section, lg, children }) => (
-  <Styled.Container section={section} lg={lg}>{children}</Styled.Container>
+const Container: React.FC<Props> = ({ section, lg, none, children }) => (
+  <Styled.Container section={section} lg={lg} none={none}>{children}</Styled.Container>
 );
 
 export default Container;
--- src/components/ui/Container/styles.ts
+++ src/components/ui/Container/styles.ts
@@ -4,11 +4,13 @@ import tw from 'tailwind.macro';
 export interface StyledProps {
   section?: boolean;
   lg?: boolean;
+  none?: boolean;
 }
 
 export const Container = styled.div<StyledProps>`
   ${tw`flex flex-wrap w-full mx-auto p-5`};
-  ${({ lg }) => !lg && tw`max-w-screen-xl`};
+  ${({ lg, none }) => !none && tw`max-w-screen-xl`};
+  ${({ lg, none }) => !lg && !none && tw`max-w-screen-xl`};
   ${({ lg }) => lg && tw`max-w-screen-lg`};
   ${({ section }) => section && tw`py-8 sm:py-16`};
 `;
